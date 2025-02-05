9ce6f19459acaa8d6d94e038551aae6f32cd1e79
Logan Boswell <loganstuartboswell@gmail.com>
2025-02-05 14:20:35 -0600

    Updated i2c Test code

--- pingpongbotlib/src/i2ctest.cpp
+++ pingpongbotlib/src/i2ctest.cpp
@@ -1,4 +1,4 @@
-#include "i2c.hpp"
+#include "pingpongbotlib/i2c.hpp"
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
@@ -23,32 +23,32 @@ int main() {
     const uint8_t motorDriverAddr = 0x34;
     const uint8_t motorTypeAddr = 0x14;
     const uint8_t motorEncoderPolarityAddr = 0x15;
-    const uint8_t motorFixedPWMAddr = 0x1F;
+    // const uint8_t motorFixedPWMAddr = 0x1F;
     const uint8_t motorFixedSpeedAddr = 0x33;
     const uint8_t motorEncoderTotalAddr = 0x3C;
-    int file = openI2CBus(i2cDevice, motorDriverAddr);
+    int file = pingpongbotlib::openI2CBus(i2cDevice, motorDriverAddr);
 
     // Set motor type (TT motor)
     uint8_t motorType = 0;  // MOTOR_TYPE_TT
-    i2cWrite(file, motorTypeAddr, &motorType, 1);
+    pingpongbotlib::i2cWrite(file, motorTypeAddr, &motorType, 1);
 
     // Set motor encoder polarity
     uint8_t motorPolarity = 1;
-    i2cWrite(file, motorEncoderPolarityAddr, &motorPolarity, 1);
+    pingpongbotlib::i2cWrite(file, motorEncoderPolarityAddr, &motorPolarity, 1);
 
     // Set motor speed (forward)
     int8_t speed[4] = {0, 0, 0, 0};  // All motors at 50 speed
-    i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)speed, 4);
+    pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)speed, 4);
 
     sleep(2);
 
     // Stop motors
     int8_t stop[4] = {0, 0, 0, 0};
-    i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)stop, 4);
+    pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)stop, 4);
 
     // Read encoder values
     uint8_t encoderData[16] = {0};
-    i2cRead(file, motorEncoderTotalAddr, encoderData, 16);
+    pingpongbotlib::i2cRead(file, motorEncoderTotalAddr, encoderData, 16);
 
     std::cout << "Encoder Readings:\n";
     for (int i = 0; i < 4; i++) {
