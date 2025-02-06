cff8883f315231582a29ea41a568c0d087d2bbd5
Logan Boswell <loganstuartboswell@gmai.com>
2025-02-06 17:25:18 -0600

    Got robot to move

--- pingpongbotlib/src/i2ctest.cpp
+++ pingpongbotlib/src/i2ctest.cpp
@@ -5,6 +5,7 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 #include <cstdint>
+#include <chrono>
 
 // #define I2C_DEVICE "/dev/i2c-1"  // I2C bus on Raspberry Pi
 // const char* i2cDevice = "/dev/i2c-1";
@@ -23,7 +24,7 @@ int main() {
     const uint8_t motorDriverAddr = 0x34;
     const uint8_t motorTypeAddr = 0x14;
     const uint8_t motorEncoderPolarityAddr = 0x15;
-    // const uint8_t motorFixedPWMAddr = 0x1F;
+    const uint8_t motorFixedPWMAddr = 0x1F;
     const uint8_t motorFixedSpeedAddr = 0x33;
     const uint8_t motorEncoderTotalAddr = 0x3C;
     int file = pingpongbotlib::openI2CBus(i2cDevice, motorDriverAddr);
@@ -33,18 +34,61 @@ int main() {
     pingpongbotlib::i2cWrite(file, motorTypeAddr, &motorType, 1);
 
     // Set motor encoder polarity
-    uint8_t motorPolarity = 1;
+    uint8_t motorPolarity = 0;
     pingpongbotlib::i2cWrite(file, motorEncoderPolarityAddr, &motorPolarity, 1);
 
     // Set motor speed (forward)
-    int8_t speed[4] = {0, 0, 0, 0};  // All motors at 50 speed
-    pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)speed, 4);
+    // int8_t speed[4] = {0, 40, 40, 0};  // All motors at 50 speed
+    // pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)speed, 4);
 
-    sleep(2);
+    // int count = 0;
+    // while (count < 1000) {
+	// count++;
+	// }
 
-    // Stop motors
-    int8_t stop[4] = {0, 0, 0, 0};
-    pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)stop, 4);
+
+    // // Stop motors
+    // int8_t stop[4] = {0, 0, 0, 0};
+    // pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)stop, 4);
+    bool done = false;
+    int i = 0;
+    bool add = true;
+    int8_t speed[4] = {0, 0, 0, 0};
+    auto start = std::chrono::high_resolution_clock::now();
+    while (!done) {
+        speed[0] = -100;
+        speed[1] = 100;
+        speed[2] = -100;
+        pingpongbotlib::i2cWrite(file, motorFixedPWMAddr, (uint8_t*)speed, 4);
+        if (add && i == 100) {
+            add = !add;
+        }
+
+        if (!add && i == -100) {
+            add = !add;
+        }
+
+        if (add) {
+            i++;
+        } else {
+            i--;
+        }
+
+        std::cout << "PWM Setting: " << i << std::endl;
+
+        auto now = std::chrono::high_resolution_clock::now();  // Current time
+        double elapsed = std::chrono::duration<double>(now - start).count();
+
+        if (elapsed >= 1) {
+            done = !done;
+        }
+        
+    }
+    speed[0] = 0;
+    speed[1] = 0;
+    speed[2] = 0;
+    pingpongbotlib::i2cWrite(file, motorFixedPWMAddr, (uint8_t*)speed, 4);
+    pingpongbotlib::i2cWrite(file, motorFixedPWMAddr, (uint8_t*)speed, 4);
 
     // Read encoder values
     uint8_t encoderData[16] = {0};
