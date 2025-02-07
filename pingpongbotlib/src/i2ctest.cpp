24f53205d72568ab2d42e55803ea82fa0c5db3b5
Logan Boswell <loganstuartboswell@gmai.com>
2025-02-07 15:12:21 -0600

    Updated i2c test

--- pingpongbotlib/src/i2ctest.cpp
+++ pingpongbotlib/src/i2ctest.cpp
@@ -1,22 +1,8 @@
 #include "pingpongbotlib/i2c.hpp"
 #include <iostream>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/i2c-dev.h>
 #include <cstdint>
 #include <chrono>
-
-// #define I2C_DEVICE "/dev/i2c-1"  // I2C bus on Raspberry Pi
-// const char* i2cDevice = "/dev/i2c-1";
-// #define MOTOR_DRIVER_ADDR 0x34   // I2C address of the motor driver
-
-// // Motor Driver Register Addresses
-// #define MOTOR_TYPE_ADDR 0x14
-// #define MOTOR_ENCODER_POLARITY_ADDR 0x15
-// #define MOTOR_FIXED_PWM_ADDR 0x1F
-// #define MOTOR_FIXED_SPEED_ADDR 0x33
-// #define MOTOR_ENCODER_TOTAL_ADDR 0x3C
 
 
 int main() {
@@ -37,44 +23,15 @@ int main() {
     uint8_t motorPolarity = 0;
     pingpongbotlib::i2cWrite(file, motorEncoderPolarityAddr, &motorPolarity, 1);
 
-    // Set motor speed (forward)
-    // int8_t speed[4] = {0, 40, 40, 0};  // All motors at 50 speed
-    // pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)speed, 4);
-
-    // int count = 0;
-    // while (count < 1000) {
-	// count++;
-	// }
-
-
-    // // Stop motors
-    // int8_t stop[4] = {0, 0, 0, 0};
-    // pingpongbotlib::i2cWrite(file, motorFixedSpeedAddr, (uint8_t*)stop, 4);
     bool done = false;
-    int i = 0;
-    bool add = true;
     int8_t speed[4] = {0, 0, 0, 0};
     auto start = std::chrono::high_resolution_clock::now();
+
     while (!done) {
-        speed[0] = -100;
-        speed[1] = 100;
-        speed[2] = -100;
+        speed[0] = -50;
+        speed[1] = 50;
+        speed[2] = -50;
         pingpongbotlib::i2cWrite(file, motorFixedPWMAddr, (uint8_t*)speed, 4);
-        if (add && i == 100) {
-            add = !add;
-        }
-
-        if (!add && i == -100) {
-            add = !add;
-        }
-
-        if (add) {
-            i++;
-        } else {
-            i--;
-        }
-
-        std::cout << "PWM Setting: " << i << std::endl;
 
         auto now = std::chrono::high_resolution_clock::now();  // Current time
         double elapsed = std::chrono::duration<double>(now - start).count();
