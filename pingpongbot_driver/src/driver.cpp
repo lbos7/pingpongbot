8136d1200077e1b3ee26b80cbfa60cb333f35886
Logan Boswell <loganstuartboswell@gmai.com>
2025-03-01 17:28:42 -0600

    Removed imu_test and added publisher for imu topic

--- pingpongbot_driver/src/driver.cpp
+++ pingpongbot_driver/src/driver.cpp
@@ -4,6 +4,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/empty.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "tf2/LinearMath/Quaternion.h"
 #include "pingpongbot_driver/interface.hpp"
 #include "pingpongbot_msgs/msg/wheel_speeds.hpp"
 #include "pingpongbot_msgs/msg/wheel_angles.hpp"
@@ -26,6 +28,8 @@ class Driver : public rclcpp::Node {
                 std::chrono::milliseconds(10), std::bind(&Driver::timerCallback, this));
 
             wheel_angles_pub_ = this->create_publisher<pingpongbot_msgs::msg::WheelAngles>("wheel_angles", 10);
+
+            imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", 10);
 
             wheel_speeds_sub_ = this->create_subscription<pingpongbot_msgs::msg::WheelSpeeds>(
                 "wheel_speeds", 10, std::bind(&Driver::wheelSpeedsCallback, this, std::placeholders::_1));
@@ -52,13 +56,43 @@ class Driver : public rclcpp::Node {
             // speeds.u2 = 60;
             // speeds.u3 = 60;
             this->interface->setSpeeds(speeds);
-            auto current_time = this->clock_.now();
+            auto current_time = this->get_clock()->now();
             if ((current_time - last_heartbeat_time).seconds() > 0.175) {
                 // RCLCPP_WARN(this->get_logger(), "Lost connection! Stopping wheels.");
                 this->interface->zeroSpeeds();
                 // driver->resetEncoderPulses();
             }
-            static pingpongbot_msgs::msg::WheelAngles prev_angles;  // Store previous wheel angles
+
+            pingpongbot_msgs::msg::IMU data = this->interface->getIMUData();
+
+            sensor_msgs::msg::Imu imu_msg;
+            imu_msg.header.stamp = this->get_clock()->now();
+            imu_msg.header.frame_id = "base_footprint";
+            imu_msg.angular_velocity.x = data.xg;
+            imu_msg.angular_velocity.y = data.yg;
+            imu_msg.angular_velocity.z = data.zg;
+            imu_msg.linear_acceleration_covariance = {6.854e-8, 0, 0, 0, 6.854e-8, 0, 0, 0, 6.854e-8};
+            imu_msg.linear_acceleration.x = data.xa;
+            imu_msg.linear_acceleration.y = data.ya;
+            imu_msg.linear_acceleration.z = data.za;
+            imu_msg.angular_velocity_covariance = {1.500625e-6, 0, 0, 0, 1.500625e-6, 0, 0, 0, 1.500625e-6};
+
+            yaw += data.zg*((current_time - prev_time).seconds());
+            yaw_variance = gyro_noise_density * gyro_noise_density * ((current_time - prev_time).seconds()) * ((current_time - prev_time).seconds());
+
+            tf2::Quaternion q;
+            q.setRPY(0.0, 0.0, yaw);
+
+            imu_msg.orientation.x = q.x();
+            imu_msg.orientation.y = q.y();
+            imu_msg.orientation.z = q.z();
+            imu_msg.orientation.w = q.w();
+
+            imu_msg.orientation_covariance = {yaw_variance, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            imu_pub_->publish(imu_msg);
+
+            // static pingpongbot_msgs::msg::WheelAngles prev_angles;  // Store previous wheel angles
 
             // RCLCPP_INFO(rclcpp::get_logger("wheel_logger"),
             //             "Wheel Angles: θ1: %.6f, θ2: %.6f, θ3: %.6f", 
@@ -79,7 +113,8 @@ class Driver : public rclcpp::Node {
             // RCLCPP_INFO(rclcpp::get_logger("wheel_logger"),
             //             "Motor Speeds: 1: %.6f, 2: %.6f, 3: %.6f", 
             //             speedInts[0], speedInts[1], speedInts[2]);
-            prev_angles = msg;
+            // prev_angles = msg;
+            prev_time = current_time;
         }
 
         void wheelSpeedsCallback(const pingpongbot_msgs::msg::WheelSpeeds & msg) {
@@ -99,9 +134,14 @@ class Driver : public rclcpp::Node {
         std::shared_ptr<pingpongbot_driver::Interface> interface;
         pingpongbot_msgs::msg::WheelSpeeds speeds;
         rclcpp::Time last_heartbeat_time;
+        double yaw = 0;
+        double yaw_variance = 0;
+        double gyro_noise_density = .0002618;
+        rclcpp::Time prev_time = this->get_clock()->now();
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Clock clock_;
         rclcpp::Publisher<pingpongbot_msgs::msg::WheelAngles>::SharedPtr wheel_angles_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
         rclcpp::Subscription<pingpongbot_msgs::msg::WheelSpeeds>::SharedPtr wheel_speeds_sub_;
         rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr heartbeat_sub_;
         rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_encoder_srv_;
